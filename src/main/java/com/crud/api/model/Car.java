fd33accc164d810ba1228389c5f997d22982cfc1
William <william.fujimori@gmail.com>
2023-01-19 17:49:30 -0300

    feat: add car model

---
+++ src/main/java/com/crud/api/model/Car.java
@@ -0,0 +1,46 @@
+package com.crud.api.model;
+
+import java.util.Date;
+
+import com.crud.api.dto.CarsDto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Car {
+
+    public Car(CarsDto data) {
+        this.modelo = data.modelo();
+        this.fabricante = data.fabricante();
+        this.dataFabricacao = data.dataFabricacao();
+        this.valor = data.valor();
+        this.anoModelo = data.anoModelo();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String modelo;
+
+    @Column(length = 50, nullable = false)
+    private String fabricante;
+
+    @Column(length = 50, nullable = false)
+    private Date dataFabricacao;
+
+    @Column(nullable = false)
+    double valor;
+
+    @Column(nullable = false)
+    int anoModelo;
+}
