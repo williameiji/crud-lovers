594104deb8fec18853d1f4ea627b010cb96fa2a4
William <william.fujimori@gmail.com>
2023-01-19 17:53:04 -0300

    feat: add delete route

--- src/main/java/com/crud/api/controller/Cars.java
+++ src/main/java/com/crud/api/controller/Cars.java
@@ -3,6 +3,7 @@ package com.crud.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,11 @@ public class Cars {
 
             return repository.save(car);
         });
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 
 }
