793bfef73b0834db518e2c8583b6696ae26c9089
William <william.fujimori@gmail.com>
2023-01-19 17:48:54 -0300

    feat: add car dto

---
+++ src/main/java/com/crud/api/dto/CarsDto.java
@@ -0,0 +1,16 @@
+package com.crud.api.dto;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+public record CarsDto(
+        @NotBlank String modelo,
+        @NotBlank String fabricante,
+        @Past Date dataFabricacao,
+        @NotNull double valor,
+        @NotNull int anoModelo) {
+
+}
